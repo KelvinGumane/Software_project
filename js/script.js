0df1109aabdfb20db39c86bc699143ffab3bf885
KelvinGumane <joaolourencogumanejunior@gmail.com>
2023-06-03 15:15:44 +0800

    update

--- js/script.js
+++ js/script.js
@@ -73,49 +73,99 @@ const validateInputs = () => {
 
 
 //login
-form.addEventListener('submit', e => {
+const logform = form.value;
+const logusername = usernameValue;
+const logemail = emailValue;
+const logpassword = passwordValue;
+
+logform.addEventListener('submit', e =>{
   e.preventDefault();
 
-  validateLogin();
+   validatelogin();
 });
 
+const logsetError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
 
-const validateLogin= validateInputs => {
-  const usernameValue = username.value.trim();
-  const emailValue = email.value.trim();
-  const passwordValue = password.value.trim();
-  const password2Value = password2.value.trim();
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success')
+}
+
+const logsetSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+
+}
+
+const validatelogin = () => {
+  const logusernameValue = logusername.value.trim()
+  const logemailValue = logemail.value.trim();
+  const logpasswordValue = logpassword.value.trim();
+ 
 
   if(usernameValue === '') {
-      setError(username, 'Username is required');
+      logsetError(username, 'Username is required');
   } else {
       setSuccess(username);
   }
 
   if(emailValue === '') {
-      setError(email, 'Email is required');
+      logsetError(email, 'Email is required');
   } else if (!isValidEmail(emailValue)) {
-      setError(email, 'Provide a valid email address');
+      logsetError(email, 'Provide a valid email address');
   } else {
       setSuccess(email);
   }
 
   if(passwordValue === '') {
-      setError(password, 'Password is required');
+      logsetError(password, 'Password is required');
   } else if (passwordValue.length < 8 ) {
-      setError(password, 'Password must be at least 8 character.')
+      logsetError(password, 'Password must be at least 8 character.')
   } else {
-      setSuccess(password);
+      logsetSuccess(password);
   }
 
   if(password2Value === '') {
-      setError(password2, 'Please confirm your password');
+      logsetError(password2, 'Please confirm your password');
   } else if (password2Value !== passwordValue) {
-      setError(password2, "Passwords doesn't match");
+      logsetError(password2, "Passwords doesn't match");
   } else {
-      setSuccess(password2);
+      logsetSuccess(password2);
   }
 
 };
 
 
+
+//shopbutton
+function shopbutton(){
+  let shopb = document.querySelector('shopb')
+  location.href ='shop.html'   
+}
+
+
+//sign in( from  first page to sign in page)
+function newuser() {
+  let newuser=document.querySelector('newuser')
+  location.href='signin.html'
+}
+
+//log in (from first page to log in page)
+function olduser(){
+  let olduser=document.querySelector('olduser')
+  location.href='login.html'
+}
+
+//Dark mode
+const modeToggle = document.getElementById('mode-toggle');
+const body = document.body;
+
+modeToggle.addEventListener('click', () => {
+body.classList.toggle('dark-mode');
+});
