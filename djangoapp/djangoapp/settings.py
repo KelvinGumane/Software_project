6d44d01633802f042e98a94f9e5e73bf03adaad8
KelvinGumane <joaolourencogumanejunior@gmail.com>
2023-06-11 15:10:04 +0800

    update

--- djangoapp/djangoapp/settings.py
+++ djangoapp/djangoapp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djangoapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'djangoapp/project/templates'],
+        'DIRS': [BASE_DIR,'djangoapp/project/html'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
